604a41c5c591581ecfe64db5b35cdf4903525897
cmd410 <stresspassing@gmail.com>
2020-06-22 23:08:54 +0300

    Fixes for disassemble operator

--- operators/disassemble_operator.py
+++ operators/disassemble_operator.py
@@ -10,6 +10,11 @@ class DisassembleOperator(bpy.types.Operator):
     bl_label = "Disassemble instance"
     bl_options = {'UNDO'}
 
+    unlink: bpy.props.BoolProperty(
+        name='Unlink objects',
+        description='New objects will have unique data'
+    )
+
     @classmethod
     def poll(cls, context):
         return all([
@@ -22,14 +27,30 @@ class DisassembleOperator(bpy.types.Operator):
 
     def execute(self, context):
         instance = context.object
+        inst_rot = context.object.rotation_euler.to_quaternion()
+
         collection = context.object.instance_collection
         for obj in collection.all_objects:
             new_obj = obj.copy()
-            if obj.type not in {'EMPTY'}:
+            if self.unlink and obj.type not in {'EMPTY'}:
                 new_obj.data = obj.data.copy()
-            new_obj.location += instance.location
+
+            # Calculate Object position
+            new_location = new_obj.location
+            new_location.rotate(inst_rot)
+            new_location += instance.location
+            new_obj.location = new_location
+
+            new_obj.rotation_mode = 'QUATERNION'
+            new_obj.rotation_quaternion.rotate(inst_rot)
+
             for c in new_obj.users_collection:
                 c.objects.unlink(new_obj)
+
             context.collection.objects.link(new_obj)
         bpy.data.objects.remove(instance, do_unlink=True)
         return {'FINISHED'}
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
